39ad5811c5ecf69563b9d6b79c9da7c5e0b246c9
Andreas Keating <aplandolit@wpi.edu>
2025-09-23 23:22:53 -0400

    A3Protect results + CRUD fixes + minor fixes and comments

--- server.express.js
+++ server.express.js
@@ -9,15 +9,16 @@ const { MongoClient, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// ---- middleware
+// ---- middleware-----
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET || 'dev-secret'));
+app.use('/results.html', requireUser); //to make sure user is loged in first befopre opening results pg
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ---- db
+// ---- db -----
 let db, Users, Entries;
 
-async function start() { // connect first, then start server
+async function start() { // Conects first and then start server
   try {
     const client = new MongoClient(process.env.MONGODB_URI);
     await client.connect();
@@ -39,8 +40,7 @@ async function start() { // connect first, then start server
 start();
 
 
-function withDerived(row) 
-{ // age from "year" field (if age in future can go negativee)
+function withDerived(row) { // age from "year" field (if age in future can go negativee)
   const currentYear = new Date().getFullYear();
   return { ...row, age: currentYear - Number(row.year) };
 }
@@ -61,7 +61,7 @@ app.post('/login', async (req, res) => {
 
   const existing = await Users.findOne({ username });
   if (!existing) {
-    await Users.insertOne({ username, password }); // minimal (plaintext for assignment)
+    await Users.insertOne({ username, password });
     res.cookie('user', username, { signed: true, httpOnly: true });
     return res.status(201).json({ username });
   }
@@ -78,16 +78,23 @@ app.post('/logout', (req, res) => {
 
 // ---- pages ----------------
 app.get('/', (req, res) => {
-  if (req.signedCookies.user) return res.redirect('/app');
+  if (req.signedCookies.user) return res.redirect('/app'); //Checks Signed in user already
   res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
+//Routs to page once user signed in
 app.get('/app', requireUser, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// protect results page too. So unauthenticated users redirected before page loads
+app.get('/results.html', requireUser, (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'results.html'));
+});
+
 // ---- API (user-scoped ) --------------
-app.get('/api/entries', requireUser, async (req, res) => {
+//chekc data of already signed in user
+app.get('/api/entries', requireUser, async (req, res) => { 
   const rows = await Entries.find({ user: req.user }).toArray();
   res.json(rows.map(withDerived));
 });
@@ -123,7 +130,7 @@ app.delete('/api/entries/:id', requireUser, async (req, res) => {
   res.json(withDerived(r.value));
 });
 
-start();
+//start();
 // app.listen(port, () => {
 //   console.log(`Express server on http://localhost:${port}`);
 // });
